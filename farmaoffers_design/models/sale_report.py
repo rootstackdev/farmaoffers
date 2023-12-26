352742ebecbc535785d324a0e45e50d1dc787a03
Michael Valencia <mvalencia@rootstack.com>
2023-12-26 14:48:42 -0500

    Cambios reporte de ventas

--- farmaoffers_design/models/sale_report.py
+++ farmaoffers_design/models/sale_report.py
@@ -4,18 +4,31 @@ from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
-
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
-    standard_price = fields.Float(string="Costo Producto", readonly=True)
+    standard_price = fields.Float(
+        string="Costo Producto",
+        readonly=True
+    )
+    
+    total_cost = fields.Float(
+        string='Total costo',
+        readonly=True
+    )
+
+    total_margin = fields.Float(
+        string='Margen total',
+        readonly=True,
+    )
+
 
     def _select_additional_fields(self, fields):
         return super()._select_additional_fields(fields)
 
     def _select_sale(self, fields=None):
         res = super()._select_sale(fields)
-        res += ",prop.value_float AS standard_price"
+        res += ",prop.value_float AS standard_price, SUM(l.qty_delivered  * prop.value_float) as total_cost, ( SUM(l.price_total) - SUM(l.qty_delivered  * prop.value_float) ) as total_margin " 
         return res
 
     def _from_sale(self, from_clause=""):
@@ -32,7 +45,7 @@ class SaleReport(models.Model):
 
     def _select_pos(self, fields=None):
         res = super()._select_pos(fields)
-        res += ",prop.value_float AS standard_price"
+        res += ",prop.value_float AS standard_price, SUM(l.qty * prop.value_float)   as total_cost, ( SUM(l.price_subtotal_incl) / MIN(CASE COALESCE(pos.currency_rate, 0) WHEN 0 THEN 1.0 ELSE pos.currency_rate END) - SUM(l.qty * prop.value_float)) as total_margin"
         return res
 
     def _from_pos(self):
@@ -43,5 +56,5 @@ class SaleReport(models.Model):
 
     def _group_by_pos(self):
         res = super()._group_by_pos()
-        res += """,prop.value_float"""
+        res += """,prop.value_float """ # ,l.qty
         return res
