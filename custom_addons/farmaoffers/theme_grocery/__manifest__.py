46f025f39322a2c12ba2e1b37273158499b94a2d
Hector Rodriguez <hrodriguez@rootstack.com>
2025-09-04 14:25:36 -0500

    WIP Adjust template

--- custom_addons/farmaoffers/theme_grocery/__manifest__.py
+++ custom_addons/farmaoffers/theme_grocery/__manifest__.py
@@ -25,6 +25,26 @@
         "views/product_details_templates.xml",
         "views/snippets.xml",
     ],
+    "assets": {
+        # Variables primarias (bootstrap + design tokens): aquí van overrides de variables
+        "web._assets_primary_variables": [
+            "theme_grocery/static/src/scss/customise_variables.scss"
+        ],
+        "website.assets_frontend": [
+            "theme_grocery/static/src/scss/style.scss",
+            "theme_grocery/static/description/favicon.ico",
+            "theme_grocery/static/lib/ion.rangeSlider-2.3.1/css/ion.rangeSlider.css",
+            "theme_grocery/static/lib/OwlCarousel/owl.carousel.min.css",
+            "theme_grocery/static/lib/malihu-custom-scrollbar/jquery.mCustomScrollbar.min.css",
+            "theme_grocery/static/src/css/style.css",
+            "theme_grocery/static/src/js/quick_view_dialog.js",
+            "theme_grocery/static/src/js/price_filter.js",
+            "theme_grocery/static/lib/ion.rangeSlider-2.3.1/js/ion.rangeSlider.js",
+            "theme_grocery/static/lib/OwlCarousel/owl.carousel.min.js",
+            "theme_grocery/static/lib/malihu-custom-scrollbar/jquery.mCustomScrollbar.concat.min.js",
+            "theme_grocery/static/src/js/script.js"
+        ]
+    },
     "demo": ["demo/theme_grocery_demo.xml"],
     "installable": True,
     "application": True,
